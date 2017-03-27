5d8918d3577cbed77b422d00f4f7fc355c3663c9
No <nomoonen@gmail.com>
2017-03-27 15:21:17 +0200

    toegevoegt dat als er nu input is dat de standaardtekst weggaat

--- WIDM ICT App/WIDM ICT App/MainActivity.cs
+++ WIDM ICT App/WIDM ICT App/MainActivity.cs
@@ -12,13 +12,32 @@ namespace WIDM_ICT_App
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
-            ///
+            SetContentView(Resource.Layout.Main);
+            //
 
-            //GREETZ
-            //hoi
-            //Test
-            //wat is dit toch leuk
+            //dit voegt toe dat de text gebruikersnaam wordt verwijdert als er op het tekstveld wordt geklikt
+            EditText gbInputField = FindViewById<EditText>(Resource.Id.gb_input);
+            gbInputField.Click += delegate
+            {
+                if (gbInputField.Text.Equals("Gebruikersnaam"))
+                //de tekst wordt alleen verwijdert als er Gebruikersnaam staat 
+                //zodat er niet per ongeluk een naam word verwijdert
+                {
+                    gbInputField.Text = "";
+                }
+            };
+
+            //dit voegt toe dat de text wachtwoord wordt verwijdert als er op het tekstveld wordt geklikt
+            EditText wwInputField = FindViewById<EditText>(Resource.Id.ww_input);
+            wwInputField.Click += delegate
+            {
+                if (wwInputField.Text.Equals("Wachtwoord"))
+                //de tekst wordt alleen verwijdert als er Wachtwoord staat 
+                //zodat er niet per ongeluk een wachtwoord word verwijdert
+                {
+                    wwInputField.Text = "";
+                }
+            };
         }
     }
 }
