cb86aaee42f738d050289b5bc8eec3e6a0e2a534
Rai <Rai@Rai>
2017-03-27 19:47:11 +0200

    layout change

--- WIDM ICT App/WIDM ICT App/Resources/Resource.Designer.cs
+++ WIDM ICT App/WIDM ICT App/Resources/Resource.Designer.cs
@@ -60,20 +60,17 @@ namespace WIDM_ICT_App
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int btn1 = 2131034114;
+			
 			// aapt resource value: 0x7f050003
-			public const int btn1 = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int btn2 = 2131034116;
-			
-			// aapt resource value: 0x7f050001
-			public const int gb_input = 2131034113;
+			public const int btn2 = 2131034115;
 			
 			// aapt resource value: 0x7f050000
-			public const int linearLayout1 = 2131034112;
+			public const int gb_input = 2131034112;
 			
-			// aapt resource value: 0x7f050002
-			public const int ww_input = 2131034114;
+			// aapt resource value: 0x7f050001
+			public const int ww_input = 2131034113;
 			
 			static Id()
 			{
