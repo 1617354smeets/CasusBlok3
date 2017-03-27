9add75f6229999d9b8dd3914adfb775ea0c3a386
Rai <Rai@Rai>
2017-03-27 14:09:03 +0200

    added comment

--- WIDM ICT App/WIDM ICT App/MainActivity.cs
+++ WIDM ICT App/WIDM ICT App/MainActivity.cs
@@ -12,7 +12,10 @@ namespace WIDM_ICT_App
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            ///SetContentView (Resource.Layout.Main);
+            ///
+
+            //GREETZ
         }
     }
 }
