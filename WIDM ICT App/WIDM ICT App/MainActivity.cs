03bae64253885e175a4e1e3e9edfe75c2f15a8c2
Rai <Rai@Rai>
2017-03-28 13:13:54 +0200

    hint toegevoegd aan de inputvelden van de inlogpagina, hierdoor was een stuk code overbodig.

--- WIDM ICT App/WIDM ICT App/MainActivity.cs
+++ WIDM ICT App/WIDM ICT App/MainActivity.cs
@@ -15,32 +15,7 @@ namespace WIDM_ICT_App
             SetContentView(Resource.Layout.Main);
             //
 
-            //dit voegt toe dat de text gebruikersnaam wordt verwijdert als er op het tekstveld wordt geklikt
-            EditText gbInputField = FindViewById<EditText>(Resource.Id.gb_input);
-            gbInputField.Click += delegate
-            {
-                if (gbInputField.Text.Equals("Gebruikersnaam"))
-                //de tekst wordt alleen verwijdert als er Gebruikersnaam staat 
-                //zodat er niet per ongeluk een naam word verwijdert
-                {
-                    gbInputField.Text = "";
-                }
-            };
-
-            //dit voegt toe dat de text wachtwoord wordt verwijdert als er op het tekstveld wordt geklikt
-            EditText wwInputField = FindViewById<EditText>(Resource.Id.ww_input);
-            wwInputField.Click += delegate
-            {
-                if (wwInputField.Text.Equals("Wachtwoord"))
-                //de tekst wordt alleen verwijdert als er Wachtwoord staat 
-                //zodat er niet per ongeluk een wachtwoord word verwijdert
-                {
-                    wwInputField.Text = "";
-                }
-
-
-                //Test
-            };
+           
 
             Button btn2 = FindViewById<Button>(Resource.Id.btn2);
             btn2.Click += delegate
