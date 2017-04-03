77958ed9252e2b92d3613824a49e91cb6234b7a6
Rai <Rai@Rai>
2017-04-03 17:56:50 +0100

    layout change

--- WIDM ICT App/WIDM ICT App/Resources/Resource.Designer.cs
+++ WIDM ICT App/WIDM ICT App/Resources/Resource.Designer.cs
@@ -73,10 +73,13 @@ namespace WIDM_ICT_App
 			public const int Error_Icon = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Icon = 2130837505;
+			public const int fgprn = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int WIDM_Icon = 2130837506;
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int WIDM_Icon = 2130837507;
 			
 			static Drawable()
 			{
