ea9644439f309203718fb437f7e0b795828f2669
Rai <Rai@Rai>
2017-04-07 13:54:30 +0200

    ip updated

--- WIDM ICT App/WIDM ICT App/Connection.cs
+++ WIDM ICT App/WIDM ICT App/Connection.cs
@@ -20,7 +20,7 @@ namespace WIDM_ICT_App
         private static Connection instance;
 
 		private int port = 50000;
-		private string IP = "192.168.0.102";
+		private string IP = "10.77.132.102";
         private Thread clientThread;
         private NetworkStream stream;
         private TcpClient client;
